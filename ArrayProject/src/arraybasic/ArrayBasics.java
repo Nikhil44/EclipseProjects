7dc8a3ca06cf8b978554db556e379b9484fa745f
Nikhil44 <nikhilfunde44@gmail.com>
2021-02-13 06:51:36 +0530

    aaaaaaaaaadffffffaaaaaaaaaaaaaaaaaaa

--- ArrayProject/src/arraybasic/ArrayBasics.java
+++ ArrayProject/src/arraybasic/ArrayBasics.java
@@ -28,6 +28,8 @@ public class ArrayBasics {
 		System.out.println("The value of array to string : "+Arrays.toString(arr2));
 		
                  System.out.println("adding new line uuu/mm/dd");
+                System.out.println("adding new line uuu/mm/dd hh:mm:ss");
+
 }
 
 }
